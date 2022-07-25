6ee3aa048e1c9500bb4a8073f2b8d809f6c86652
ZhenLong <neslxzhen@gmail.com>
2022-07-25 17:43:00 +0800

    init

---
+++ app/src/main/java/self/nesl/local_news_receiver/MainActivity.kt
@@ -0,0 +1,43 @@
+package self.nesl.local_news_receiver
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import self.nesl.local_news_receiver.ui.theme.LocalnewsreceiverTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LocalnewsreceiverTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LocalnewsreceiverTheme {
+        Greeting("Android")
+    }
+}
