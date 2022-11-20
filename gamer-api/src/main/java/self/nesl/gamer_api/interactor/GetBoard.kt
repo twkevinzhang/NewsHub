c8c6db73c726d4ba0ea2e9a23db45191c1cb7220
ZhenLong <neslxzhen@gmail.com>
2022-11-20 19:20:13 +0800

    fix(gamer-api): NoSuchElementException

--- gamer-api/src/main/java/self/nesl/gamer_api/interactor/GetBoard.kt
+++ gamer-api/src/main/java/self/nesl/gamer_api/interactor/GetBoard.kt
@@ -2,10 +2,9 @@ package self.nesl.gamer_api.interactor
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import self.nesl.gamer_api.model.GBoard
 
 class GetBoard {
     suspend fun invoke(url: String) = withContext(Dispatchers.IO) {
-        GetAllBoard().invoke().first { it.url == url }
+        GetAllBoard().invoke().first { url.contains(it.url) }
     }
 }
