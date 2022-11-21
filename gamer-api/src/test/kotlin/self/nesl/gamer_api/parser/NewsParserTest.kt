84b16fcb6e7faa2b04f919df23dcd98b889e7a2d
ZhenLong <neslxzhen@gmail.com>
2022-11-21 19:53:40 +0800

    fix(gamer-api.test): Fill unit test

---
+++ gamer-api/src/test/kotlin/self/nesl/gamer_api/parser/NewsParserTest.kt
@@ -0,0 +1,19 @@
+package self.nesl.gamer_api.parser
+
+import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import self.nesl.gamer_api.loadFile
+
+internal class NewsParserTest {
+
+    @Test
+    fun `Test NewsParser expect successful`() {
+        val parser = NewsParser()
+        val news = parser.parse(
+            Jsoup.parse(loadFile("./src/test/html/News.html")),
+            "https://forum.gamer.com.tw/C.php?bsn=60076",
+        )
+        assertEquals(news.title, "【問題】吃滷味/麻辣燙只點麵、肉片、菜，然後不點火鍋料跟豆製食品的人在想什麼啊的人")
+    }
+}
--- gamer-api/src/test/kotlin/self/nesl/gamer_api/parser/UrlParserTest.kt
+++ gamer-api/src/test/kotlin/self/nesl/gamer_api/parser/UrlParserTest.kt
@@ -10,13 +10,6 @@ import org.junit.jupiter.api.Test
 internal class UrlParserTest {
 
     @Test
-    fun `Test parsePostId expect successful`() {
-        val parser = UrlParserImpl()
-        val postId = parser.parsePostId("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245".toHttpUrl())
-        assertEquals("87956245", postId)
-    }
-
-    @Test
     fun `Test parseBoardId expect successful`() {
         val parser = UrlParserImpl()
         val boardId = parser.parseBoardId("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245".toHttpUrl())
@@ -28,5 +21,40 @@ internal class UrlParserTest {
         val parser = UrlParserImpl()
         val threadId = parser.parseThreadId("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245".toHttpUrl())
         assertEquals("7398008", threadId)
+    }
+
+    @Test
+    fun `Test parsePostId expect successful`() {
+        val parser = UrlParserImpl()
+        val postId = parser.parsePostId("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245".toHttpUrl())
+        assertEquals("87956245", postId)
+    }
+
+    @Test
+    fun `Test parsePage expect successful`() {
+        val parser = UrlParserImpl()
+        val page = parser.parsePage("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245&page=2".toHttpUrl())
+        assertEquals(2, page)
+    }
+
+    @Test
+    fun `Test hasPostId expect successful`() {
+        val parser = UrlParserImpl()
+        val hasPostId = parser.hasPostId("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245".toHttpUrl())
+        assertEquals(true, hasPostId)
+    }
+
+    @Test
+    fun `Test hasPage expect successful`() {
+        val parser = UrlParserImpl()
+        val hasPage = parser.hasPage("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245&page=2".toHttpUrl())
+        assertEquals(true, hasPage)
+    }
+
+    @Test
+    fun `Test parseThreadUrl expect successful`() {
+        val parser = UrlParserImpl()
+        val url = parser.parseThreadUrl("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008&sn=87956245&page=2".toHttpUrl())
+        assertEquals("https://forum.gamer.com.tw/C.php?bsn=60076&snA=7398008", url)
     }
 }
